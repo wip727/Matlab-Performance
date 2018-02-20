992809f0db1d05e954f83b7e525665021e249762
wip727 <wip727@gmail.com>
2018-02-20 14:23:37 -0500

    Add files via upload

---
+++ examplePerfTest_NoLabel.m
@@ -0,0 +1,19 @@
+classdef examplePerfTest_NoLabel < matlab.perftest.TestCase
+    methods(Test)
+        function testSVD1(testCase)
+            testCase.startMeasuring()
+            X = rand(1000);
+            testCase.stopMeasuring()
+        end
+        function testSVD2(testCase)
+            sz = 732;
+            X = rand(sz);
+            testCase.startMeasuring()
+            [U,S,V] = svd(X);
+            testCase.stopMeasuring()
+            testCase.verifyTrue(isdiag(S))
+            testCase.verifyTrue(issorted(diag(S),'descend'))
+            testCase.verifySize(S,[sz sz 1])
+        end
+    end
+end
